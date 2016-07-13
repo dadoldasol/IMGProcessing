0a09f5ffcdabb37c02c8c2a59cce119b4774b048
unknown <dasol8297@gmail.com>
2016-07-13 16:53:26 +0900

    CHAPTER 6

---
+++ ImageTool/IppImage/IppEnhance.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "IppImage.h"
+
+void IppInverse(IppByteImage& img);
+void IppBrightness(IppByteImage& img, int n);
+void IppContrast(IppByteImage& img, int n);
+void IppGammaCorrection(IppByteImage& img, float gamma);
+void IppHistogram(IppByteImage& img, float histo[256]);
+void IppHistogramStretching(IppByteImage& img);
+void IppHistogramEqualization(IppByteImage& img);
